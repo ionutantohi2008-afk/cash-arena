221bd0b0a0c9f423bcaf1dc3d1ca7c5912d15be1
ionutantohi2008-afk <ionutantohi2008@gmail.com>
2026-05-26 19:07:16 +0200

    Add files via upload

--- league.js
+++ league.js
@@ -44,7 +44,7 @@ async function loadLeague() {
   // Affiche le rank en haut
   document.getElementById("playerRank")
     .innerHTML =
-      `🏆 Rank : ${getRankBadge(currentRank)} ${currentRank}`;
+      `🏆 Rank : (currentRank)} ${currentRank}`;
 
   // Récupère tous les users
   const snapshot = await db
@@ -98,7 +98,7 @@ async function loadLeague() {
 
           ${p.pseudo || p.email}
 
-          ${getRankBadge(p.leagueRank)}
+          (p.leagueRank)}
 
           ${p.isContentCreator
             ? "<span class='creator-badge'>Content Creator</span>"
